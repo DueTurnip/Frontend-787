d54b20f78e05c74c30b57b0f1419ac4df18df469
DueTurnip <110312263+DueTurnip@users.noreply.github.com>
2026-02-23 17:41:27 -0500

    fin

--- module6_solution/js/app.js
+++ module6_solution/js/app.js
@@ -6,7 +6,38 @@
 
     LunchController.$inject = ['$scope'];
     function LunchController($scope){
-        // Do stuff
+        $scope.count = 0;
+        $scope.message = "";
+        $scope.list = "";
+        $scope.color = "";
+
+        $scope.LunchMessage = function () {
+            // Reset message cause it just keeps adding
+            $scope.message = ""
+            $scope.count = 0;
+
+            calculateCount($scope.list);
+            if ($scope.count == 0) {
+                $scope.message = "Please enter data first";
+                $scope.color = "error";
+            } else if ($scope.count > 3) {
+                $scope.message = "Too much!";
+                $scope.color = "error";
+            } else {
+                $scope.message = "Enjoy!";
+                $scope.color = "succ";
+            };
+        }
+
+        function calculateCount (string) {
+            var words = string.split(",");
+            for (var i = 0; i < words.length; i++) {
+                words[i] = words[i].trim();
+                if (words[i] != ""){
+                    $scope.count += 1;
+                }
+            };
+        }
     };
 
 })();
