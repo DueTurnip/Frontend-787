24fdf072e075e465538273933b86b321bc7605e4
DueTurnip <110312263+DueTurnip@users.noreply.github.com>
2026-03-03 16:15:03 -0500

    final functionality

--- module7-solution/js/app.js
+++ module7-solution/js/app.js
@@ -4,7 +4,12 @@
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
-        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+        .filter('customCurrency', function() {
+            return function(amount) {
+                return '$$$' + amount.toFixed(2);
+            }
+        });
 
 
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
@@ -31,21 +36,21 @@
 
         var bought = [];
         var toBuy = [
-            { name: "Bumble Bees", quantity: 30 },
-            { name: "Honey Bees", quantity: 10 },
-            { name: "Miner Bees", quantity: 15 },
-            { name: "Plumber Bees", quantity: 3 },
-            { name: "Leafcutter Bees", quantity: 22 },
-            { name: "Carpenter Bees", quantity: 8 },
-            { name: "Mason Bees", quantity: 9 },
-            { name: "Sweat Bees", quantity: 34 }
+            { name: "Bumble Bees", quantity: 30, pricePerItem: 2 },
+            { name: "Honey Bees", quantity: 10, pricePerItem: 3 },
+            { name: "Miner Bees", quantity: 15, pricePerItem: 4 },
+            { name: "Plumber Bees", quantity: 3, pricePerItem: 5 },
+            { name: "Leafcutter Bees", quantity: 22, pricePerItem: 5 },
+            { name: "Carpenter Bees", quantity: 8, pricePerItem: 4 },
+            { name: "Mason Bees", quantity: 9, pricePerItem: 3 },
+            { name: "Sweat Bees", quantity: 34, pricePerItem: 2 }
         ];
 
         service.buyItem = function(item) {
             var index = toBuy.findIndex(i => i.name === item.name);
             toBuy.splice(index, 1);
             bought.push(item);
-        };
+        }
 
         service.getBought = function() {
             return bought;
